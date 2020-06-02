9b30fa0872a61a77d5462cafbf5f2caadb734a74
goondrious <srs_34@hotmail.com>
2020-06-02 15:07:37 -0400

    install jq and add extra check

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ COPY package.json ./
 COPY ./test ./test
 COPY ./dash-page.js ./
 COPY ./wait-for-grid.sh ./
+RUN apt-get update
+RUN apt-get install -y jq
 RUN yarn
 
 #TODO: dockerize the whole app and test?
--- wait-for-grid.sh
+++ wait-for-grid.sh
@@ -7,11 +7,16 @@ host=$1
 nodes=$2
 cmd=$3
 
-# until curl -sSL "${host}/status" 2>$1 | jq -r "value.ready" | grep "true" >/dev/null; do
-until curl -sSL "${host}/grid/api/hub" | jq -r ".slotCounts.total" | grep -q "${nodes}"; do
-  echo "Selenium not found"
+until curl -sSL "${host}/wd/hub/status" | jq -r ".value.ready" | grep -q "true"; do
+  echo "Selenium not found!"
   sleep 1
 done
 
->&2 echo "Selenium is up - executing command"
+# until curl -sSL "${host}/status" 2>$1 | jq -r "value.ready" | grep "true" >/dev/null; do
+until curl -sSL "${host}/grid/api/hub" | jq -r ".slotCounts.total" | grep -q "${nodes}"; do
+  echo "Nodes not found!"
+  sleep 1
+done
+
+echo "Selenium is up - executing command"
 exec $cmd
